69d1cd7135aab03030901efc598b76f423b94b4a
nkolbow <30391241+nkolbow@users.noreply.github.com>
2018-04-06 17:29:41 -0500

    Added comments to Graph.java

--- p4/Graph.java
+++ p4/Graph.java
@@ -1,152 +1,158 @@
 import java.util.ArrayList;
 
 /**
- * Undirected and unweighted graph implementation
+ * An undirected and unweighted graph.
  * 
- * @param <E> type of a vertex
- * 
- * @author sapan (sapan@cs.wisc.edu)
- * 
+ * @author Nathan
+ *
+ * @param <E>
+ *            the vertex's unique identifier
  */
 public class Graph<E> implements GraphADT<E> {
-    
-    /**
-     * Instance variables and constructors
-     */
-	
+
+	// the graph's adjacency matrix
 	private boolean[][] am;
+	
+	// list of all vertices, their indices correspond to the vertex indices on the
+	// adjacency matrix
 	private ArrayList<E> vertices = new ArrayList<E>();
-	
-	public Graph() {
-		
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public E addVertex(E vertex) {
+		if (vertices.contains(vertex) || vertex == null)
+			return null;
+
+		incrementAM();
+		vertices.add(vertex);
+		return vertex;
 	}
-	
+
+	/**
+	 * Increases the size of the adjacency matrix by 1.
+	 */
 	private void incrementAM() {
-		if(am == null) {
+		if (am == null) {
 			am = new boolean[1][1];
 		} else {
 			boolean[][] temp = new boolean[am.length + 1][am.length + 1];
-			for(int y = 0; y < am.length; y++) {
-				for(int x = 0; x < am.length; x++) {
+			for (int y = 0; y < am.length; y++) {
+				for (int x = 0; x < am.length; x++) {
 					temp[y][x] = am[y][x];
 				}
 			}
 			am = temp;
 		}
 	}
-	
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public E removeVertex(E vertex) {
+		if (!vertices.contains(vertex))
+			return null;
+
+		int _vertex = vertices.indexOf(vertex);
+		decrementAM(_vertex);
+		vertices.remove(vertex);
+		return vertex;
+	}
+
+	/**
+	 * Decreases the size of the adjacency matrix by 1, removing the specified
+	 * vertex's row and column.
+	 * 
+	 * @param _remove
+	 *            the index of the vertex being removed
+	 */
 	private void decrementAM(int _remove) {
 		boolean[][] temp = new boolean[am.length - 1][am.length - 1];
-		for(int y = 0; y < am.length - 1; y++) {
-			if(y == _remove)
+		for (int y = 0; y < am.length - 1; y++) {
+			if (y == _remove)
 				continue;
-			for(int x = 0; x < am.length - 1; x++) {
-				if(x == _remove)
+			for (int x = 0; x < am.length - 1; x++) {
+				if (x == _remove)
 					continue;
 				temp[y][x] = am[(y > _remove) ? y - 1 : y][(x > _remove) ? x - 1 : x];
 			}
 		}
 	}
-	
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public E addVertex(E vertex) {
-        if(vertices.contains(vertex) || vertex == null)
-        	return null;
-        
-        incrementAM();
-        vertices.add(vertex);
-        return vertex;
-    }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public E removeVertex(E vertex) {
-    	if(!vertices.contains(vertex))
-    		return null;
-    	
-        int _vertex = vertices.indexOf(vertex);
-    	decrementAM(_vertex);
-    	vertices.remove(vertex);
-    	return vertex;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean addEdge(E vertex1, E vertex2) {
+		if (vertex1 == vertex2)
+			return false;
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean addEdge(E vertex1, E vertex2) {
-    	if(vertex1 == vertex2)
-    		return false;
-    	
-        int _vertex1 = vertices.indexOf(vertex1);
-        int _vertex2 = vertices.indexOf(vertex2);
-        if(_vertex1 < 0 || _vertex2 < 0)
-        	return false;
-        
-        am[_vertex1][_vertex2] = true;
-        am[_vertex2][_vertex1] = true;
-        return true;
-    }    
+		int _vertex1 = vertices.indexOf(vertex1);
+		int _vertex2 = vertices.indexOf(vertex2);
+		if (_vertex1 < 0 || _vertex2 < 0)
+			return false;
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean removeEdge(E vertex1, E vertex2) {
-    	if(vertex1 == vertex2)
-    		return false;
-    	
-        int _vertex1 = vertices.indexOf(vertex1);
-        int _vertex2 = vertices.indexOf(vertex2);
-        if(_vertex1 < 0 || _vertex2 < 0)
-        	return false;
-        
-        am[_vertex1][_vertex2] = false;
-        am[_vertex2][_vertex1] = false;
-        return true;
-    }
+		am[_vertex1][_vertex2] = true;
+		am[_vertex2][_vertex1] = true;
+		return true;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean isAdjacent(E vertex1, E vertex2) {
-        int _vertex1 = vertices.indexOf(vertex1);
-        int _vertex2 = vertices.indexOf(vertex2);
-        
-        if(_vertex1 == -1 || _vertex2 == -1)
-        	return false;
-        
-        return am[_vertex1][_vertex2];
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean removeEdge(E vertex1, E vertex2) {
+		if (vertex1 == vertex2)
+			return false;
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Iterable<E> getNeighbors(E vertex) {
-        ArrayList<E> out = new ArrayList<E>();
-        int _vertex = vertices.indexOf(vertex);
-        for(int i = 0; i < am.length; i++) {
-        	if(i == _vertex)
-        		continue;
-        	if(am[_vertex][i])
-        		out.add(vertices.get(i));
-        }
-        return out;
-    }
+		int _vertex1 = vertices.indexOf(vertex1);
+		int _vertex2 = vertices.indexOf(vertex2);
+		if (_vertex1 < 0 || _vertex2 < 0)
+			return false;
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Iterable<E> getAllVertices() {
-        return vertices;
-    }
+		am[_vertex1][_vertex2] = false;
+		am[_vertex2][_vertex1] = false;
+		return true;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isAdjacent(E vertex1, E vertex2) {
+		int _vertex1 = vertices.indexOf(vertex1);
+		int _vertex2 = vertices.indexOf(vertex2);
+
+		if (_vertex1 == -1 || _vertex2 == -1)
+			return false;
+
+		return am[_vertex1][_vertex2];
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Iterable<E> getNeighbors(E vertex) {
+		ArrayList<E> out = new ArrayList<E>();
+		int _vertex = vertices.indexOf(vertex);
+		for (int i = 0; i < am.length; i++) {
+			if (i == _vertex)
+				continue;
+			if (am[_vertex][i])
+				out.add(vertices.get(i));
+		}
+		return out;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Iterable<E> getAllVertices() {
+		return vertices;
+	}
 
 }
