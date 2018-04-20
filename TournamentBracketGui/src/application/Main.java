856ecd9db92a98239cf38a3c00916c30d2a3c903
nkolbow <30391241+nkolbow@users.noreply.github.com>
2018-04-20 16:46:57 -0500

    local

--- TournamentBracketGui/src/application/Main.java
+++ TournamentBracketGui/src/application/Main.java
@@ -16,39 +16,34 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,1920,1080);
 			
-			int numberOfTeams = 16;
+			int numberOfTeams = 8;
 			VersusBox[] matches = new VersusBox[numberOfTeams - 1];
 			// create matches
 			HBox bracketBox = new HBox();
-			VBox[] round = new VBox[4];
-			int divider = 2;
-			int matchesCounter = 0;
+			VBox[] round = new VBox[3];
+			int vboxHeight = 134;
+			int divisor = 2;
 			for (int i = 0; i < round.length; i++) {
-				round[i] = new VBox();
-				round[i].setMinHeight(380);
-				round[i].setMaxHeight(600);
-				round[i].setPrefHeight(390);
-				round[i].setStyle("-fx-background-color: #FFFFFF;");
-				for (int j = 0; j < (numberOfTeams / divider); j++) {
-//					matches[matchesCounter].setPadding(new Insets(20, 10, 20, 10));
-//					round[i].getChildren().add(matches[matchesCounter]);
-					Label a = new Label("Hello");
-					int numberOfVersusBoxes = numberOfTeams / divider;
-					double verticalSpacing = (600 - (numberOfVersusBoxes * 20)) / (numberOfVersusBoxes + 1);
-//					a.setText(Double.toString(round[i].getHeight()));
-					verticalSpacing = verticalSpacing / 2;
-					a.setPadding(new Insets(verticalSpacing, 10, verticalSpacing, 10));
-					round[i].getChildren().add(a);
-					matchesCounter++;
+				VBox box = new VBox();
+				box.setMinHeight(700);
+				box.setMaxHeight(720);
+				box.setPrefHeight(710);
+				round[i] = box;
+				
+				double padding = (scene.getHeight() - (vboxHeight * numberOfTeams/divisor))/(numberOfTeams+1);
+				for(int j = 0; j < numberOfTeams/divisor; j++) {
+					VersusBox vbox = new VersusBox(TypeOfMatch.NORMAL_GAME, new Team("asdf", vboxHeight), new Team("fdsa", (int)padding));
+					vbox.setPadding(new Insets(padding));
+					round[i].getChildren().add(vbox);
+					divisor *= 2;
 				}
 				bracketBox.getChildren().add(round[i]);
-				divider *= 2;
 			}
 			
-			BorderPane root = new BorderPane();
 			root.setCenter(bracketBox);
-			Scene scene = new Scene(root,400,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
