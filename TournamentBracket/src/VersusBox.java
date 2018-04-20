c1fc79c70edd4117add75191ce019c2dcf80d5a8
Zach <torus@10.141.68.12>
2018-04-20 14:54:14 -0500

    1st push

---
+++ TournamentBracket/src/VersusBox.java
@@ -0,0 +1,30 @@
+import javafx.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
+public class VersusBox extends VBox {
+  int teamScore1;
+  int teamScore2;
+
+  HBox team1Box;
+  HBox team2Box;
+  
+  HBox btnBox;
+  
+  Label team1Lbl;
+  Label team2Lbl;
+  
+  TextField team1TxtField;
+  TextField team2TxtField;
+  
+  Button submitBtn;
+  
+  public VersusBox() {
+    
+  }
+  
+  public VersusBox(int something) {
+    
+  }
+
+}
