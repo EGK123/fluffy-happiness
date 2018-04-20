32abb80434124f6da48e3679ad80dbc3e8e38993
Zach <torus@10.141.68.12>
2018-04-20 15:07:49 -0500

    enum

---
+++ TournamentBracket/src/TypeOfMatch.java
@@ -0,0 +1,4 @@
+
+public enum TypeOfMatch {
+  NORMAL_GAME, QUARTER_FINAL, SEMI_FINAL, GRAND_CHAMPIONSHIP
+}
--- TournamentBracket/src/VersusBox.java
+++ TournamentBracket/src/VersusBox.java
@@ -8,23 +8,37 @@ public class VersusBox extends VBox {
 
   HBox team1Box;
   HBox team2Box;
-  
+
   HBox btnBox;
-  
+
   Label team1Lbl;
   Label team2Lbl;
-  
+
   TextField team1TxtField;
   TextField team2TxtField;
-  
+
   Button submitBtn;
-  
+
   public VersusBox() {
+    int teamScore1 = 0;
+    int teamScore2 = 0;
+    
+    HBox team1Box = null;
+    HBox team2Box = null;
+    HBox btnBox = null;
+    
+    team1Lbl = null;
+    team2Lbl = null;
+    
+    team1TxtField = null;
+    team2TxtField = null;
+    
+    submitBtn = null;
     
   }
-  
+
   public VersusBox(int something) {
-    
+
   }
 
 }
