5fa59bc7348913d8d8e63e05582295e4bf9022c9
Patrick Lacina <placina@wisc.edu>
2018-05-02 13:33:06 -0500

    Added comment headers to all .java files

--- TournamentBracketGui/src/application/VersusBox.java
+++ TournamentBracketGui/src/application/VersusBox.java
@@ -1,3 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester: CS400 Spring 2018
+// PROJECT: Team Project, Milestone 3
+// FILES: Main.java
+// Scoreable.java
+// Team.java
+// TypeOfMatch.java
+// VersusBox.java
+// application.css
+// teams.txt
+//
+// Authors: Zach Kremer, Ege Kula, Patrick Lacina, Nathan Kolbow, Jong Kim
+// Due date: 10:00 PM on Thursday, May 3rd
+// Outside sources: None
+//
+// Instructor: Deb Deppeler (deppeler@cs.wisc.edu)
+// Bugs: No known bugs
+//
+//////////////////////////// 80 columns wide //////////////////////////////////
+
 package application;
 
 import javafx.scene.layout.*;
